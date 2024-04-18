50aafd4b24d202309f14aec60cf9fb794a9b603a
Baxic <WstActGitHub@gmail.com>
2024-04-18 03:48:39 -0400

    EJ-refactorRegion
    Need to change searchRecord(fsColName)

--- src/main/java/org/guanzon/cas/parameters/Region.java
+++ src/main/java/org/guanzon/cas/parameters/Region.java
@@ -158,8 +158,8 @@ public class Region implements GRecord{
                                         , lsSQL
                                         , fsValue
                                         , "Code»Name"
-                                        , "xBankName»xBankCode»xTownName"
-                                        , "xBankName»xBankCode»xTownName"
+                                        , "sRegionID»sRegionNm"
+                                        , "sRegionID»sRegionNm"
                                         , fbByCode ? 0 : 1);
     }
     
