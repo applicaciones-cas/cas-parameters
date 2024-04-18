92c49aac3e98eb22f702a046be9af44cd9569356
Baxic <WstActGitHub@gmail.com>
2024-04-18 02:33:25 -0400

    EJ-refactorBanksBranches
    Need to change searchRecord(fsColName)

--- src/main/java/org/guanzon/cas/parameters/Banks_Branches.java
+++ src/main/java/org/guanzon/cas/parameters/Banks_Branches.java
@@ -158,8 +158,8 @@ public class Banks_Branches implements GRecord{
                                         , lsSQL
                                         , fsValue
                                         , "Code»Name"
-                                        , "xBankName»xBankCode»xTownName"
-                                        , "xBankName»xBankCode»xTownName"
+                                        , "sBrBankID»sBrBankNm"
+                                        , "sBrBankID»sBrBankNm"
                                         , fbByCode ? 0 : 1);
     }
     
