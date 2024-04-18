67ab9f8e60bb9854ef447b649934280dbdf69400
Baxic <WstActGitHub@gmail.com>
2024-04-18 03:48:58 -0400

    EJ-refactorRelationship
    Need to change searchRecord(fsColName)

--- src/main/java/org/guanzon/cas/parameters/Relationship.java
+++ src/main/java/org/guanzon/cas/parameters/Relationship.java
@@ -158,8 +158,8 @@ public class Relationship implements GRecord{
                                         , lsSQL
                                         , fsValue
                                         , "Code»Name"
-                                        , "xBankName»xBankCode»xTownName"
-                                        , "xBankName»xBankCode»xTownName"
+                                        , "sRelatnID»sRelatnDs"
+                                        , "sRelatnID»sRelatnDs"
                                         , fbByCode ? 0 : 1);
     }
     
