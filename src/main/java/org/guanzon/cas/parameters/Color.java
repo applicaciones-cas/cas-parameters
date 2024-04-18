f2593dc7336ca13d2b0e2c4a360d3f64e067347c
Baxic <WstActGitHub@gmail.com>
2024-04-18 02:58:38 -0400

    EJ-refactorColor
    Need to change searchRecord(fsColName)

--- src/main/java/org/guanzon/cas/parameters/Color.java
+++ src/main/java/org/guanzon/cas/parameters/Color.java
@@ -158,8 +158,8 @@ public class Color implements GRecord{
                                         , lsSQL
                                         , fsValue
                                         , "Code»Name"
-                                        , "xBankName»xBankCode»xTownName"
-                                        , "xBankName»xBankCode»xTownName"
+                                        , "sColorCde»sDescript"
+                                        , "sColorCde»sDescript"
                                         , fbByCode ? 0 : 1);
     }
     
