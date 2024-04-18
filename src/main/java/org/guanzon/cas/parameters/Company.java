7e29755ec45d584332a0c0099dd086b5bc7355a9
Baxic <WstActGitHub@gmail.com>
2024-04-18 02:59:26 -0400

    EJ-refactorCompany
    Need to change searchRecord(fsColName)

--- src/main/java/org/guanzon/cas/parameters/Company.java
+++ src/main/java/org/guanzon/cas/parameters/Company.java
@@ -158,8 +158,8 @@ public class Company implements GRecord{
                                         , lsSQL
                                         , fsValue
                                         , "Code»Name"
-                                        , "xBankName»xBankCode»xTownName"
-                                        , "xBankName»xBankCode»xTownName"
+                                        , "sCompnyID»sCompnyNm"
+                                        , "sCompnyID»sCompnyNm"
                                         , fbByCode ? 0 : 1);
     }
     
