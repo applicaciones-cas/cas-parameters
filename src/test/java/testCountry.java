d2dab4f34aaed22e3f3a3d4c971f7751d39d7263
mark <117447312+Markluis09@users.noreply.github.com>
2024-04-22 14:50:02 +0800

    Markluis-testCountry
    Start: April 18 2024
    Done: April 22 2024

---
+++ src/test/java/testCountry.java
@@ -0,0 +1,60 @@
+import org.guanzon.appdriver.base.GRider;
+import org.guanzon.appdriver.base.MiscUtil;
+import org.guanzon.cas.parameters.Country;
+import org.json.simple.JSONObject;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class testCountry {
+    static GRider instance;
+    static Country record;
+    
+    @BeforeClass
+    public static void setUpClass(){
+        System.setProperty("sys.default.path.metadata", "D:/GGC_Maven_Systems/config/metadata/");
+        
+        instance = MiscUtil.Connect();
+        record = new Country(instance, false);
+    }
+    
+   
+    
+    @Test
+    public void testProgramFlow(){
+        JSONObject loJSON;
+        
+        loJSON = record.newRecord();
+        if ("error".equals((String) loJSON.get("result"))) Assert.fail((String) loJSON.get("message"));
+        
+        loJSON = record.getModel().setCntryNme("US");
+        if ("error".equals((String) loJSON.get("result"))) Assert.fail((String) loJSON.get("message"));
+        
+          
+        loJSON = record.getModel().setCntryCde("33");
+        if ("error".equals((String) loJSON.get("result"))) Assert.fail((String) loJSON.get("message"));
+        
+        
+        loJSON = record.getModel().setNational("Filipino");
+        if ("error".equals((String) loJSON.get("result"))) Assert.fail((String) loJSON.get("message"));
+        
+        loJSON = record.getModel().setModifiedBy(instance.getUserID());
+        if ("error".equals((String) loJSON.get("result"))) Assert.fail((String) loJSON.get("message"));
+        
+        loJSON = record.getModel().setModifiedDate(instance.getServerDate());
+        if ("error".equals((String) loJSON.get("result"))) Assert.fail((String) loJSON.get("message"));
+        
+        loJSON = record.saveRecord();
+        if ("error".equals((String) loJSON.get("result"))) Assert.fail((String) loJSON.get("message"));
+    }
+    
+     @AfterClass
+    public static void tearDownClass() {
+        record = null;
+        instance = null;
+    }
+}
