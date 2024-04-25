eee0421ac252aa10eab6440f19bead6ca5ef9e8a
mark <117447312+Markluis09@users.noreply.github.com>
2024-04-25 14:00:20 +0800

    Markluis/Banks_Branches
    Start: April 24, 2024
    Done: April 25, 2024

--- src/test/java/testBanks_Branches.java
+++ src/test/java/testBanks_Branches.java
@@ -1,3 +1,4 @@
+
 import org.guanzon.appdriver.base.GRider;
 import org.guanzon.appdriver.base.MiscUtil;
 import org.guanzon.cas.parameters.Banks_Branches;
@@ -12,46 +13,90 @@ import org.junit.runners.MethodSorters;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class testBanks_Branches {
+
     static GRider instance;
     static Banks_Branches record;
-    
+
     @BeforeClass
-    public static void setUpClass(){
+    public static void setUpClass() {
         System.setProperty("sys.default.path.metadata", "D:/GGC_Maven_Systems/config/metadata/");
-        
+
         instance = MiscUtil.Connect();
         record = new Banks_Branches(instance, false);
     }
-    
-   
-    
+
     @Test
-    public void testProgramFlow(){
+    public void testProgramFlow() {
         JSONObject loJSON;
-        
+
          loJSON = record.newRecord();
-        if ("error".equals((String) loJSON.get("result"))) Assert.fail((String) loJSON.get("message"));
+        if ("error".equals((String) loJSON.get("result"))) {
+            Assert.fail((String) loJSON.get("message"));
+        }
+
+//        loJSON = record.getModel().setBranchesBanksID("435");
+//        if ("error".equals((String) loJSON.get("result"))) {
+//            Assert.fail((String) loJSON.get("message"));
+//        }
+
+        loJSON = record.getModel().setBranchesBanksName("MMB");
+        if ("error".equals((String) loJSON.get("result"))) {
+            Assert.fail((String) loJSON.get("message"));
+        }
         
-        loJSON = record.getModel().setBrBankNm("Bank of the Philippine Islands");
-        if ("error".equals((String) loJSON.get("result"))) Assert.fail((String) loJSON.get("message"));
+         loJSON = record.getModel().setBranchesBanksCoDe("MMB");
+        if ("error".equals((String) loJSON.get("result"))) {
+            Assert.fail((String) loJSON.get("message"));
+        }
         
-        loJSON = record.getModel().setBrBankCD("BPI");
-        if ("error".equals((String) loJSON.get("result"))) Assert.fail((String) loJSON.get("message"));
+         loJSON = record.getModel().setBanksID("MMB");
+        if ("error".equals((String) loJSON.get("result"))) {
+            Assert.fail((String) loJSON.get("message"));
+        }
         
-        loJSON = record.getModel().setBankIDxx("");
-        if ("error".equals((String) loJSON.get("result"))) Assert.fail((String) loJSON.get("message"));
+        loJSON = record.getModel() .setBranchesBanksID("MMB");
+        if ("error".equals((String) loJSON.get("result"))) {
+            Assert.fail((String) loJSON.get("message"));
+        }
         
+        loJSON = record.getModel() .setAddress("MMB");
+        if ("error".equals((String) loJSON.get("result"))) {
+            Assert.fail((String) loJSON.get("message"));
+        }
+        
+        loJSON = record.getModel() .setTownID("MMB");
+        if ("error".equals((String) loJSON.get("result"))) {
+            Assert.fail((String) loJSON.get("message"));
+        }
+        
+        loJSON = record.getModel() .setTelephoneNumber("MMB");
+        if ("error".equals((String) loJSON.get("result"))) {
+            Assert.fail((String) loJSON.get("message"));
+        }
+        
+         loJSON = record.getModel() .setFaxNumber("MMB");
+        if ("error".equals((String) loJSON.get("result"))) {
+            Assert.fail((String) loJSON.get("message"));
+        }
+
         loJSON = record.getModel().setModifiedBy(instance.getUserID());
-        if ("error".equals((String) loJSON.get("result"))) Assert.fail((String) loJSON.get("message"));
-        
+        if ("error".equals((String) loJSON.get("result"))) {
+            Assert.fail((String) loJSON.get("message"));
+        }
+
         loJSON = record.getModel().setModifiedDate(instance.getServerDate());
-        if ("error".equals((String) loJSON.get("result"))) Assert.fail((String) loJSON.get("message"));
-        
+        if ("error".equals((String) loJSON.get("result"))) {
+            Assert.fail((String) loJSON.get("message"));
+        }
+
         loJSON = record.saveRecord();
-        if ("error".equals((String) loJSON.get("result"))) Assert.fail((String) loJSON.get("message"));
+        if ("error".equals((String) loJSON.get("result"))) {
+            Assert.fail((String) loJSON.get("message"));
+        }
+
     }
-    
-     @AfterClass
+
+    @AfterClass
     public static void tearDownClass() {
         record = null;
         instance = null;
