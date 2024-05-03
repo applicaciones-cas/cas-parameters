97719ff8f2f318b947ade5f9832b030aba913f1a
mark <117447312+Markluis09@users.noreply.github.com>
2024-05-03 16:19:58 +0800

    Markluis-refactorCategory
    April 26, 2024
    May 3, 2024

--- src/main/java/org/guanzon/cas/parameters/Category.java
+++ src/main/java/org/guanzon/cas/parameters/Category.java
@@ -64,7 +64,7 @@ public class Category implements GRecord {
 
     @Override
     public JSONObject openRecord(String fsValue) {
-        return poModel.openRecord("sCategrCd = " + SQLUtil.toSQL(fsValue));
+        return poModel.openRecord("sBrandCde = " + SQLUtil.toSQL(fsValue));
     }
 
     @Override
@@ -176,7 +176,7 @@ public class Category implements GRecord {
 
         if (poJSON
                 != null) {
-            return poModel.openRecord((String) poJSON.get("sCategrCd"));
+            return poModel.openRecord((String) poJSON.get("sBrandCde"));
         } else {
             poJSON.put("result", "error");
             poJSON.put("message", "No record loaded to update.");
