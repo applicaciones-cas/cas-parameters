e93eb9221257c6fc41f38d55dda81edabdb01b64
Aldristh456034 <162650996+Aldristh456034@users.noreply.github.com>
2024-05-21 13:15:49 +0800

    Changed

---
+++ src/main/java/org/guanzon/cas/parameters/Term.java
@@ -0,0 +1,188 @@
+package org.guanzon.cas.parameters;
+
+import org.guanzon.appdriver.agent.ShowDialogFX;
+import org.guanzon.appdriver.base.GRider;
+import org.guanzon.appdriver.base.MiscUtil;
+import org.guanzon.appdriver.base.SQLUtil;
+import org.guanzon.appdriver.constant.EditMode;
+import org.guanzon.appdriver.iface.GRecord;
+import org.guanzon.cas.model.parameters.Model_Size;
+import org.json.simple.JSONObject;
+
+public class Term implements GRecord {
+
+    GRider poGRider;
+    boolean pbWthParent;
+    int pnEditMode;
+    String psRecdStat;
+
+    Model_Size poModel;
+    JSONObject poJSON;
+
+    public Term(GRider foGRider, boolean fbWthParent) {
+        poGRider = foGRider;
+        pbWthParent = fbWthParent;
+
+        poModel = new Model_Size(foGRider);
+        pnEditMode = EditMode.UNKNOWN;
+    }
+
+    @Override
+    public void setRecordStatus(String fsValue) {
+        psRecdStat = fsValue;
+    }
+
+    @Override
+    public int getEditMode() {
+        return pnEditMode;
+    }
+
+    @Override
+    public JSONObject setMaster(int fnCol, Object foData) {
+        return poModel.setValue(fnCol, foData);
+    }
+
+    @Override
+    public JSONObject setMaster(String fsCol, Object foData) {
+        return poModel.setValue(fsCol, foData);
+    }
+
+    @Override
+    public Object getMaster(int fnCol) {
+        return poModel.getValue(fnCol);
+    }
+
+    @Override
+    public Object getMaster(String fsCol) {
+        return poModel.getValue(fsCol);
+    }
+
+    @Override
+    public JSONObject newRecord() {
+        return poModel.newRecord();
+    }
+
+    @Override
+    public JSONObject openRecord(String fsValue) {
+        return poModel.openRecord(fsValue);
+    }
+
+    @Override
+    public JSONObject updateRecord() {
+        JSONObject loJSON = new JSONObject();
+
+        if (poModel.getEditMode() == EditMode.UPDATE) {
+            loJSON.put("result", "success");
+            loJSON.put("message", "Edit mode has changed to update.");
+        } else {
+            loJSON.put("result", "error");
+            loJSON.put("message", "No record loaded to update.");
+        }
+        return loJSON;
+    }
+
+    @Override
+    public JSONObject saveRecord() {
+        if (!pbWthParent) {
+            poGRider.beginTrans();
+        }
+
+        poJSON = poModel.saveRecord();
+
+        if ("success".equals((String) poJSON.get("result"))) {
+            if (!pbWthParent) {
+                poGRider.commitTrans();
+            }
+        } else {
+            if (!pbWthParent) {
+                poGRider.rollbackTrans();
+            }
+        }
+        return poJSON;
+    }
+
+    @Override
+    public JSONObject deleteRecord(String fsValue) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public JSONObject deactivateRecord(String fsValue) {
+        poJSON = new JSONObject();
+
+        if (poModel.getEditMode() == EditMode.UPDATE) {
+            poJSON = poModel.setActive(false);
+
+            if ("error".equals((String) poJSON.get("result"))) {
+                return poJSON;
+            }
+
+            poJSON = poModel.saveRecord();
+        } else {
+            poJSON = new JSONObject();
+            poJSON.put("result", "error");
+            poJSON.put("message", "No record loaded to update.");
+        }
+        return poJSON;
+    }
+
+    @Override
+    public JSONObject activateRecord(String fsValue) {
+        poJSON = new JSONObject();
+
+        if (poModel.getEditMode() == EditMode.UPDATE) {
+            poJSON = poModel.setActive(true);
+
+            if ("error".equals((String) poJSON.get("result"))) {
+                return poJSON;
+            }
+
+            poJSON = poModel.saveRecord();
+        } else {
+            poJSON = new JSONObject();
+            poJSON.put("result", "error");
+            poJSON.put("message", "No record loaded to update.");
+        }
+        return poJSON;
+    }
+
+    @Override
+    public JSONObject searchRecord(String fsValue, boolean fbByCode) {
+        String lsCondition = "";
+
+        if (psRecdStat.length() > 1) {
+            for (int lnCtr = 0; lnCtr <= psRecdStat.length() - 1; lnCtr++) {
+                lsCondition += ", " + SQLUtil.toSQL(Character.toString(psRecdStat.charAt(lnCtr)));
+            }
+
+            lsCondition = "cRecdStat IN (" + lsCondition.substring(2) + ")";
+        } else {
+            lsCondition = "cRecdStat = " + SQLUtil.toSQL(psRecdStat);
+        }
+
+        String lsSQL = MiscUtil.addCondition(poModel.makeSelectSQL(), " sSizeName LIKE "
+                + SQLUtil.toSQL(fsValue + "%") + " AND " + lsCondition);
+
+        poJSON = ShowDialogFX.Search(poGRider,
+                lsSQL,
+                fsValue,
+                "Code»Name",
+                "sSizeIDxx»sSizeName",
+                "sSizeIDxx»sSizeName",
+                fbByCode ? 0 : 1);
+
+        if (poJSON != null) {
+            return poModel.openRecord((String) poJSON.get("sSizeIDxx"));
+        } else {
+            poJSON.put("result", "error");
+            poJSON.put("message", "No record loaded to update.");
+            return poJSON;
+        }
+    }
+
+//additional methods
+    @Override
+    public Model_Size getModel() {
+        return poModel;
+    }
+}
---
+++ src/test/java/testTerm.java
@@ -0,0 +1,62 @@
+
+import org.guanzon.appdriver.base.GRider;
+import org.guanzon.appdriver.base.MiscUtil;
+import org.guanzon.cas.parameters.Size;
+import org.json.simple.JSONObject;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class testTerm {
+
+    static GRider instance;
+    static Size record;
+
+    @BeforeClass
+    public static void setUpClass() {
+        System.setProperty("sys.default.path.metadata", "D:/GGC_Maven_Systems/config/metadata/");
+
+        instance = MiscUtil.Connect();
+        record = new Size(instance, false);
+    }
+
+    @Test
+    public void testProgramFlow() {
+        JSONObject loJSON;
+
+        loJSON = record.newRecord();
+        if ("error".equals((String) loJSON.get("result"))) {
+            Assert.fail((String) loJSON.get("message"));
+        }
+
+        loJSON = record.getModel().setSizeName("KKK");
+        if ("error".equals((String) loJSON.get("result"))) {
+            Assert.fail((String) loJSON.get("message"));
+        }
+
+        loJSON = record.getModel().setModifiedBy(instance.getUserID());
+        if ("error".equals((String) loJSON.get("result"))) {
+            Assert.fail((String) loJSON.get("message"));
+        }
+
+        loJSON = record.getModel().setModifiedDate(instance.getServerDate());
+        if ("error".equals((String) loJSON.get("result"))) {
+            Assert.fail((String) loJSON.get("message"));
+        }
+
+        loJSON = record.saveRecord();
+        if ("error".equals((String) loJSON.get("result"))) {
+            Assert.fail((String) loJSON.get("message"));
+        }
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        record = null;
+        instance = null;
+    }
+}
